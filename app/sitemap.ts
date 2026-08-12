11cb0276c3519ddac292e3b364dc06bd8a2d2601
hskshe954 <jhancostante@gmail.com>
2026-08-12 16:24:47 +0800

    Update sitemap to custom domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://fearlessbeautylounge.vercel.app",
+      url: "https://www.nursemitchfearlessbeautylounge.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
